a2bbf35a4b9726266b97c1a83bc9a6b30326ac9e
MarcosYH <marcosdeimos18@gmail.com>
2023-02-24 10:54:35 +0100

    initilisation

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import '../styles/App.css'; 
+
+function App() {
+  const title = "Projet adewale"
+  const soustitle= "commençons"
+  return (
+    
+    <div className="App">
+      <header className="App-header">
+        <h1 className='lmj-title'>{title}</h1>
+        <h2>{soustitle}</h2> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
