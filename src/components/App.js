0fc6ec37011cf81a000b5c7ff196f056176985fc
MarcosYH <marcosdeimos18@gmail.com>
2023-02-27 11:13:37 +0100

    update title

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,33 @@
-//import Interfaceweb from './Interfaceweb'; 
-import '../styles/App.css';
-import Header from './Header';
-import Navbar from './Navbar';
-import Apropos from './Apropos.js';
-import Nosproduits from './Nosproduits';
-import Temoignages from './Temoignages';
-import Contact from './Contact';
-import Footer from './Footer';
-import ScrollToTop from './ScrollToTop';
+//import Interfaceweb from './Interfaceweb';
+import "../styles/App.css";
+import Header from "./Header";
+import Navbar from "./Navbar";
+import Apropos from "./Apropos.js";
+import Nosproduits from "./Nosproduits";
+import Temoignages from "./Temoignages";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
+import {Helmet} from "react-helmet";
 
 function App() {
-  
   return (
     <div>
-    <Navbar/>
-    <ScrollToTop/>
-    <Header/>
-    <Apropos/>
-    <Nosproduits/>
-    <Temoignages/>
-    <Contact/>
-    <Footer/>
-		</div>
+      <Helmet>
+          <meta charSet="utf-8" />
+        <title>Adewale</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Adewale votre site de achat de vêtement." />
+      </Helmet>
+      <Navbar />
+      <ScrollToTop />
+      <Header />
+      <Apropos />
+      <Nosproduits />
+      <Temoignages />
+      <Contact />
+      <Footer />
+    </div>
   );
 }
 document.body.style.backgroundColor = "#ffffff";
